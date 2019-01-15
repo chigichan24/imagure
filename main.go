738fecf1fd62a70a6ea02caca9c0d8d1f29603a5
Kazuki Chigita <kazuki.chigichan@gmail.com>
2019-01-15 16:56:56 +0900

    error handle

--- main.go
+++ main.go
@@ -3,7 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
 )
+
+func importImage(p string) ([]float32,error) {
+	file, err := os.Open(p)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return nil, nil
+}
 
 func main() {
 	var(
@@ -11,5 +22,15 @@ func main() {
 		c = flag.String("compare", "compare.img", "the path of image which compare to original one")
 	)
 	flag.Parse()
-	fmt.Println(*o, *c)
+	fmt.Printf("original => %s, compare => %s",*o ,*c)
+
+
+	if _,err := importImage(*o); err != nil {
+		log.Fatal(err)
+	}
+
+	if _,err := importImage(*c); err != nil {
+		log.Fatal(err)
+	}
+
 }
