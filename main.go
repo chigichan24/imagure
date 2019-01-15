81e8265a34930155abe7c7709f2963b952d182c9
Kazuki Chigita <kazuki.chigichan@gmail.com>
2019-01-15 16:32:21 +0900

    parse args

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 func main() {
-	println("hello")
+	var(
+		o = flag.String("original", "original.img","the path of original image")
+		c = flag.String("compare", "compare.img", "the path of image which compare to original one")
+	)
+	flag.Parse()
+	fmt.Println(*o, *c)
 }
